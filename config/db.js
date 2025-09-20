393279a0b898bdb0203d8a11eb70ac8fa9aa0f82
Shreyance Bodh <shreyancebodh@gmail.com>
2025-09-20 14:31:41 +0530

    MONGO_URL udpated

--- config/db.js
+++ config/db.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
+  const MONGO_URL = process.env.MONGO_URL;
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/jwtAuth");
+    await mongoose.connect(MONGO_URL);
     console.log("DB connected");
   } catch (error) {
     console.log("Error in DB connection", error)
